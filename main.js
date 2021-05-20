abd8c7d6b5acb62c32b2348c8b11a36c0cca35ef
Raul <rey_brujo@hotmail.com>
2021-05-20 21:03:49 +0900

    start ES6+ migration

--- main.js
+++ main.js
@@ -1,50 +1,42 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
-var roleMaintenancer = require('role.maintenancer');
-var spawnControl = require('spawn.control');
-var generalOverview = require('general.overview');
-var rolePickV1 = require('role.pick-v1');
-var roleCarryV1 = require('role.carry-v1');
+//var roleHarvester = require('./roles/role.harvester')
+var roleUpgrader = require('./roles/role.upgrader')
+var roleBuilder = require('./roles/role.builder')
+var roleMaintenancer = require('./roles/role.maintenancer')
+var spawnControl = require('spawn.control')
+var generalOverview = require('./info/general.overview')
+var rolePickV1 = require('./roles/role.pick-v1')
+var roleCarryV1 = require('./roles/role.carry-v1')
+import Harvester from './roles/harvester'
 
 module.exports.loop = function () {
+  spawnControl.run()
+  generalOverview.run()
 
-    spawnControl.run();
-    generalOverview.run();
-
-    
-
-    for(var name in Game.creeps) {
-        /*
+  for (var name in Game.creeps) {
+    /*
         if(EVENT_ATTACK == true){
             console.log('Attacked');
         }*/
 
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-        if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
-        }
-        if(creep.memory.role == 'maintenancer') {
-            roleMaintenancer.run(creep);
-        }
-        if(creep.memory.role == 'pickS1' || creep.memory.role == 'pickS2') {
-            rolePickV1.run(creep);
-        }
-        if(creep.memory.role == 'carry') {
-            roleCarryV1.run(creep);
-        }
-        
-        
-
+    var creep = Game.creeps[name]
+    if (creep.memory.role == 'harvester') {
+      //roleHarvester.run(creep)
+      Harvester()
     }
-
-
+    if (creep.memory.role == 'upgrader') {
+      roleUpgrader.run(creep)
+    }
+    if (creep.memory.role == 'builder') {
+      roleBuilder.run(creep)
+    }
+    if (creep.memory.role == 'maintenancer') {
+      roleMaintenancer.run(creep)
+    }
+    if (creep.memory.role == 'pickS1' || creep.memory.role == 'pickS2') {
+      rolePickV1.run(creep)
+    }
+    if (creep.memory.role == 'carry') {
+      roleCarryV1.run(creep)
+    }
+  }
 }
-
-
